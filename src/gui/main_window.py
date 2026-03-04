87a45bb2bed86158f6a230295042f1d0433bb77f
Andrea Lizzio <andrealizzio2000@gmail.com>
2026-03-05 00:55:54 +0100

    feat: spostamento e creazione di gui-complete, con aggiunta del codice di main_window e login_window

---
+++ src/gui/main_window.py
@@ -0,0 +1,25 @@
+"""Modulo per la finestra principale dell'applicazione Wordle."""
+
+# pylint: disable=no-member, c-extension-no-member
+import os
+import sys
+from PyQt6 import QtWidgets, uic
+
+
+class MainWindow(QtWidgets.QMainWindow):  # pylint: disable=too-few-public-methods
+    """Classe che gestisce l'interfaccia principale."""
+
+    def __init__(self, nome_giocatore="Andrea"):
+        super().__init__()
+        ui_path = os.path.join(os.path.dirname(__file__), "main_window.ui")
+        uic.loadUi(ui_path, self)
+        self.btn_exit.clicked.connect(self.close)
+
+        self.lbl_welcome.setText(f"Bentornato/a, {nome_giocatore}!")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
