30b0441514d8ef8c39d15f83a6af61ac5c57a488
LilyBlueberry <lillianferla@gmail.com>
2026-03-06 19:27:13 +0100

    primo metodo

--- src/core/game.py
+++ src/core/game.py
@@ -1,3 +1,10 @@
-#prova
+target = "cane"
+guess = input("Inserisci parola: ")
 
-print("parola")
+for i in range(len(target)):
+    if guess[i] == target[i]:
+        print("CORRECT")
+    elif guess[i] in target:
+        print("PRESENT")
+    else:
+        print("ABSENT") 
