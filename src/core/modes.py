b15b1c70927ef70bd1145cbc4048172a1638951b
LilyBlueberry <lillianferla@gmail.com>
2026-03-10 21:10:20 +0100

    Fix: metodo classico

--- src/core/modes.py
+++ src/core/modes.py
@@ -1,4 +1,4 @@
-"""Metodi di Gioco: ClassicMode and TrainingMode."""
+"""Modalità di Gioco: ClassicMode and TrainingMode."""
 
 import datetime
 import logging
@@ -11,8 +11,13 @@ from game import Game
 
 logger = logging.getLogger(__name__)
 
-
+# Metodo Classico: una parola al giorno
 class ClassicMode:
-    """
-    ClassicMode: una parola al giorno
-    """
+    
+    #costruttore
+    def __init__(self, word_provider: WordProvider) -> None:
+        
+        self.word_provider = word_provider
+        self.game: Optional[Game] = None
+        self.score: int = 0
+        self.current_word: Optional[str] = None
