367e06776575f23e4288f4d462901704f1479039
LilyBlueberry <lillianferla@gmail.com>
2026-03-11 15:31:38 +0100

    Aggiunta classe Gestioni delle modalità

--- src/core/game_manager.py
+++ src/core/game_manager.py
@@ -1,1 +1,22 @@
-# Gestione delle partite
+from modes import ClassicMode, TrainingMode
+from word_provider import WordProvider
+
+class GameManager:
+    """
+    Coordina il gioco, scegliendo la modalità e collegando
+    le chiamate della UI alla logica del gioco.
+    """
+
+    def __init__(self, word_provider: WordProvider):
+        self.word_provider = word_provider
+        self.mode = None  # ClassicMode o TrainingMode
+
+    # Avvia modalità classica (parola del giorno + punteggio)
+    def start_classic(self):
+        self.mode = ClassicMode(self.word_provider)
+        self.mode.start()
+
+    # Avvia modalità allenamento (parole random, infinite, senza punteggio)
+    def start_training(self):
+        self.mode = TrainingMode(self.word_provider)
+        self.mode.start()
