8d67e61ae129c853158d1b9cc97b3bb6227286b0
Andrea Lizzio <andrealizzio2000@gmail.com>
2026-03-19 20:47:42 +0100

    feat(gui): modifica in _controlla_parola per bug delle valueError parole mancanti nel dizionario

--- src/gui/game_window.py
+++ src/gui/game_window.py
@@ -296,12 +296,13 @@ class GameWindow(BaseWindow):
             print("Errore: GameManager non collegato!")
             return
 
-        risultati = self.game_manager.submit_guess(tentativo)
-
-        if risultati is None:
-            # Mostra un messaggio all'utente o semplicemente ignora l'invio
+        try:
+            risultati = self.game_manager.submit_guess(tentativo)
+        except ValueError:
             QtWidgets.QMessageBox.warning(
-                self, "Parola non valida", "La parola non è presente nel dizionario."
+                self, 
+                "Parola non valida", 
+                f"La parola '{tentativo}' non è presente nel dizionario."
             )
             return
 
